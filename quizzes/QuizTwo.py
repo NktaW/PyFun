8c9280caec0f4b2a624f145094fe6009c88c6ca0
unknown <nikita.waara@hotmail.com>
2021-10-26 16:05:59 +0300

    saving changes

---
+++ quizzes/QuizTwo.py
@@ -0,0 +1,32 @@
+#Start
+
+def check_guess(guess, answer):
+    global score
+    still_guessing = True
+    attempt = 0
+    while still_guessing and attempt < 3:
+        if guess.lower() == answer.lower():
+            print('Congraz! Your answer is correct.')
+            score = score + 1
+            still_guessing = False
+        else:
+            if attempt < 2:
+                guess = input('Sorry wrong answer, plase try again')
+                attempt = attempt + 1
+    if attempt == 3:
+        print('The right answer is ' + answer)
+
+#Create a score variable 
+score = 0
+
+#Start quiz, show questions for user
+print('Guess the right answer')
+
+#Create question
+guess1 = input('How many hearts does Octopus have ? \n \
+A) Three\n B) One\n C) Seven\n \
+        What is your answer ? A, B, or C')
+check_guess(guess1, 'A')
+
+#Show score to user
+print('Your score is ' + str(score))
