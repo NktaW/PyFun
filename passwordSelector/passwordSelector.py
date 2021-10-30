94a918444f3430efe13e8c94fdadd55594728de2
unknown <nikita.waara@hotmail.com>
2021-10-30 10:20:26 +0300

    saving changes

--- passwordSelector/passwordSelector.py
+++ passwordSelector/passwordSelector.py
@@ -22,16 +22,17 @@ print('Hi there, and welcome to choose a new strong password!')
 
 #Make a while loop that user can have multiple options to choose.
 while True:
+    for num in range(3):
 #Choose word using choise() function, from the pythons random module.
-    adjective = random.choice(adjectives)
-    noun = random.choice(nouns) 
+        adjective = random.choice(adjectives)
+        noun = random.choice(nouns) 
 #Choose a random number using randrange() function, from random module
-    number = random.randrange(0, 100)
+        number = random.randrange(0, 100)
 #Choose a special character using random.choise() function to make a password stronger
-    special_char = random.choice(string.punctuation)
+        special_char = random.choice(string.punctuation)
 #Assemble all parts and create strong password
-    password = adjective + noun + str(number) + special_char
-    print('Here is you\'re new password: %s' % password)
+        password = adjective + noun + str(number) + special_char
+        print('Here is you\'re new password: %s' % password)
 
     response = input('Want a new password ?  Type y or n:  ')
     if response == 'n':
