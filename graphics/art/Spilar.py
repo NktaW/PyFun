c5c4009de60dad562c8b4c49ff642abb84d1fab2
unknown <nikita.waara@hotmail.com>
2021-11-14 11:10:37 +0200

    gg

--- graphics/art/Spilar.py
+++ graphics/art/Spilar.py
@@ -3,14 +3,16 @@ from itertools import cycle
 
 colors = cycle(['red', 'green', 'orange', 'yellow', 'blue', 'purple'])
 
+bg_colors = cycle(['Purple', 'Peru', 'Forest Green', 'goldenrod', 'Hot pink' ])
 
 
 def draw_circle(size, angle, shift):
+    t.bgcolor(next(bg_colors))
     t.pencolor(next(colors))
     t.circle(size)
     t.right(angle)
     t.forward(shift)
-    draw_circle(size + 5, angle +1, shift + 1)
+    draw_circle(size + 10, angle + 10, shift + 1)
     
     
 t.bgcolor('black')
