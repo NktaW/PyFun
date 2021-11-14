b68d15540fe1dea4a3bc8cbde4af5dc08e516a9e
unknown <nikita.waara@hotmail.com>
2021-11-14 10:58:22 +0200

    saving changes

--- graphics/art/Spilar.py
+++ graphics/art/Spilar.py
@@ -5,20 +5,18 @@ colors = cycle(['red', 'green', 'orange', 'yellow', 'blue', 'purple'])
 
 
 
-def draw_circle(size):
+def draw_circle(size, angle, shift):
     t.pencolor(next(colors))
     t.circle(size)
-    draw_circle(size + 5)
-    
-    
-    
-    
+    t.right(angle)
+    t.forward(shift)
+    draw_circle(size + 5, angle +1, shift + 1)
     
     
 t.bgcolor('black')
 t.speed('fast')
-t.pensize(4)
+t.pensize(20)
 
-draw_circle(30)
+draw_circle(30, 0, 1)
 
 
