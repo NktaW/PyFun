e7bc9e30fb6fd5157f0d739f29672515c0417aef
unknown <nikita.waara@hotmail.com>
2021-11-15 11:13:45 +0200

    saving changes

--- graphics/art/Spilar.py
+++ graphics/art/Spilar.py
@@ -6,19 +6,27 @@ colors = cycle(['red', 'green', 'orange', 'yellow', 'blue', 'purple'])
 bg_colors = cycle(['Purple', 'Peru', 'Forest Green', 'goldenrod', 'Hot pink' ])
 
 
-def draw_circle(size, angle, shift):
-    t.bgcolor(next(bg_colors))
+def draw_shape(size, angle, shift, shape):
+    #t.bgcolor(next(bg_colors))
     t.pencolor(next(colors))
-    t.circle(size)
+    next_shape = ''
+    if shape == 'circle':
+        t.circle(size)
+        next_shape = 'square'
+    elif shape == 'square':
+        for i in range(4):
+            t.forward(size * 2)
+            t.left(90)
+        next_shape = 'circle'
     t.right(angle)
     t.forward(shift)
-    draw_circle(size + 10, angle + 10, shift + 1)
+    draw_shape(size + 5, angle + 1, shift + 1, next_shape)
     
     
 t.bgcolor('black')
 t.speed('fast')
 t.pensize(20)
 
-draw_circle(30, 0, 1)
+draw_shape(30, 0, 1, 'circle')
 
 
